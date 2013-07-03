ba77d8a142f53a5e950a2ac2d017b7082b8a3f62
spmno <sunqingpeng@hotmail.com>
2013-07-03 02:40:30 -0700

    no change

---
+++ MenuOrder/Data/DataManager.h
@@ -0,0 +1,13 @@
+//
+//  DataManager.h
+//  MenuOrder
+//
+//  Created by spmno on 13-7-3.
+//  Copyright (c) 2013年 spmno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataManager : NSObject
++ (DataManager*) sharedInstance;
+@end
