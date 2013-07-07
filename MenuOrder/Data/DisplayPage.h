c044f2804ffe23c033c4604563f0c92ed998bb45
spmno <sunqingpeng@hotmail.com>
2013-07-08 07:04:23 +0800

    create displaypage for data

---
+++ MenuOrder/Data/DisplayPage.h
@@ -0,0 +1,13 @@
+//
+//  DisplayPage.h
+//  MenuOrder
+//
+//  Created by spmno on 13-7-8.
+//  Copyright (c) 2013年 spmno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DisplayPage : NSObject
+
+@end
---
+++ MenuOrder/Data/DisplayPage.m
@@ -0,0 +1,13 @@
+//
+//  DisplayPage.m
+//  MenuOrder
+//
+//  Created by spmno on 13-7-8.
+//  Copyright (c) 2013年 spmno. All rights reserved.
+//
+
+#import "DisplayPage.h"
+
+@implementation DisplayPage
+
+@end
